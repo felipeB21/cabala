1d7da0ee0d2d79a07f67f272bcf06c942e1e63ff
Felipe Bolgar <bolgarfelipe@gmail.com>
2026-05-22 16:14:53 -0300

    feat: add sizes attribute to user image for improved responsiveness

--- components/match-detail.tsx
+++ components/match-detail.tsx
@@ -420,6 +420,7 @@ export function MatchDetailClient({
                       src={c.user.image}
                       alt={c.user.name ?? ""}
                       fill
+                      sizes="32px"
                       className="object-cover"
                     />
                   ) : (
